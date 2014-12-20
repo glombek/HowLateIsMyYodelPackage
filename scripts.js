0daa8f2a8f573128bbf2785f89a342d6bc64bdec
Joe Glombek <gh@joe.gl>
2014-12-20 11:16:55 +0000

    initial

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function CalculateTime() {
+    var worstCaseScheduled = moment('2014-12-04 21:00');
+    var bestCaseArrival = moment();
+    var duration = moment.duration(bestCaseArrival.diff(worstCaseScheduled));
+    
+    $('.time--days').text(Math.round(duration.asDays()));
+    $('.time--hours').text(duration.hours());
+    $('.time--minutes').text(duration.minutes());
+    $('.time--seconds').text(duration.seconds());
+}
+
+CalculateTime();
+setInterval(CalculateTime, 1000);
