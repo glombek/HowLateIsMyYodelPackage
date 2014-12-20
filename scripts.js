7c89675062b5500f6f70746af4d367af023c33eb
Joe Glombek <gh@joe.gl>
2014-12-20 23:40:42 +0000

    pluralisation

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
 function CalculateTime() {
+    function display(num, singular) {
+        if(num == 1) {
+            return num + ' ' + singular;
+        } else {
+            return num + ' ' + singular + 's';
+        }
+    }
+    
     var worstCaseScheduled = moment('2014-12-04 21:00');
     var bestCaseArrival = moment();
     var duration = moment.duration(bestCaseArrival.diff(worstCaseScheduled));
     
-    $('.time--days').text(Math.floor(duration.asDays()));
-    $('.time--hours').text(duration.hours());
-    $('.time--minutes').text(duration.minutes());
-    $('.time--seconds').text(duration.seconds());
+    $('.time--days').text(display(Math.floor(duration.asDays()), 'day'));
+    $('.time--hours').text(display(duration.hours(), 'hour'));
+    $('.time--minutes').text(display(duration.minutes(), 'minute'));
+    $('.time--seconds').text(display(duration.seconds(), 'second'));
 }
 
 CalculateTime();
